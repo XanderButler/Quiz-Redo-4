191555b4f8f2f0ab5f3fe8e68d05d61ac864aa2f
XanderButler <143623555+XanderButler@users.noreply.github.com>
2024-02-02 14:51:31 -0600

    Create circle.cpp

---
+++ circle.cpp
@@ -0,0 +1,29 @@
+#include "circle.h"
+#include <iostream>
+
+// Default constructor
+Circle::Circle() : x(0), y(0), radius(1.0) {}
+
+// Methods
+void Circle::move(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
+
+void Circle::resize(float radius) {
+    this->radius = radius;
+}
+
+void Circle::read() {
+    std::cout << "Enter x coordinate: ";
+    std::cin >> x;
+    std::cout << "Enter y coordinate: ";
+    std::cin >> y;
+    std::cout << "Enter radius: ";
+    std::cin >> radius;
+}
+
+void Circle::print() {
+    std::cout << "Center: (" << x << ", " << y << ")\n";
+    std::cout << "Radius: " << radius << "\n";
+}
